6b278af13541c51f194ca133c3937262cbc2ff35
Kolya <raloz1@gmail.com>
2016-12-07 22:05:52 +0400

    Change share text

--- application/views/step_2.php
+++ application/views/step_2.php
@@ -19,13 +19,9 @@
 				<span><?php echo lang('share_and_tag'); ?></span>
 			</div>
 			<br>
-			<div class="message">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			<div class="message text-left">
+				<h3>GPC SACHUQARI</h3>
+				<p>მე დავსაჩუქრდი ჯიპისისგან 40 ქულით. ჯიპისი საჩუქრების ჩუქების ადგილი</p>
 			</div>
 			<div>
 				<button id="share-button" class="btn btn-primary button">
--- static/js/step_2.js
+++ static/js/step_2.js
@@ -4,8 +4,10 @@ $(function(){
 		FB.ui({
 			method: 'feed',
 			link: url.base,
-			name: 'Test',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			name: $('.message > h3').html(),
+			description: $('.message > p').html(),
+			caption: '#GPCSACHUQARI',
+			hashtag: '#GPCSACHUQARI',
 		}, function(response){
 
 			if(response && !response.error_code) {
